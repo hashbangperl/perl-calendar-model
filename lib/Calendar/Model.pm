a8d3b6faf0c90c902f4997a48d9b927baa327b34
Aaron Trevena <aaron.trevena@gmail.com>
2012-12-29 15:55:02 +0000

    initial import of Module::Starter stub

---
+++ lib/Calendar/Model.pm
@@ -0,0 +1,111 @@
+package Calendar::Model;
+
+use 5.006;
+use strict;
+use warnings FATAL => 'all';
+
+=head1 NAME
+
+Calendar::Model - The great new Calendar::Model!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Calendar::Model;
+
+    my $foo = Calendar::Model->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Aaron Trevena, C<< <teejay at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-calendar-model at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Calendar-Model>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Calendar::Model
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Calendar-Model>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Calendar-Model>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Calendar-Model>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Calendar-Model/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Aaron Trevena.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See L<http://dev.perl.org/licenses/> for more information.
+
+
+=cut
+
+1; # End of Calendar::Model
