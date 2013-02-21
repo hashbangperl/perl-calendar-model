209a4d84d2dc070d67fdbf043a369ef2dc7cc537
Aaron Trevena <aaron.trevena@gmail.com>
2013-02-21 21:15:52 +0000

    Added examples

--- examples/calendar.pl
+++ examples/calendar.pl
@@ -8,14 +8,20 @@ use Data::Dumper;
 use DateTime;
 use Calendar::Model;
 
-my $cal = Calendar::Model->new(selected_date=>DateTime->new(day=>3, month=>1, year=>2013));
+my $cal = Calendar::Model->new(selected_date=>DateTime->new(day=>5, month=>12, year=>2012));
 
-warn Dumper($cal->columns);
+my $events = {
+    '2012-12-16' => 'Mousehole Lights',
+    '2012-12-19' => 'Anniversary of Penlee Lifeboat Disaster',
+};
 
-warn Dumper $cal->weeks;
+my $holidays = {
+    '2012-12-25' => 'Christmas Day',
+    '2012-12-26' => 'Boxing Day',
+};
 
 # create Template object
 my $template = Template->new({ INCLUDE_PATH => 'templates', POST_CHOMP=> 1,});
 # process input template, substituting variables
-$template->process('calendar.tt', {cal => $cal })
+$template->process('calendar.tt', {cal => $cal, events => $events, holidays => $holidays })
 || die $template->error();
