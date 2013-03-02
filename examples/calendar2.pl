10dc822f84e70ae93b53dc2fba806b3c806908ec
Aaron Trevena <aaron.trevena@gmail.com>
2013-03-02 16:57:45 +0000

    added new wordpress widget style calendar example

--- examples/calendar2.pl
+++ examples/calendar2.pl
@@ -3,7 +3,6 @@
 use strict;
 
 use Template;
-
 use DateTime;
 use Calendar::Model;
 
@@ -14,12 +13,6 @@ my $events = {
     '2012-12-19' => 'Anniversary of Penlee Lifeboat Disaster',
 };
 
-my $holidays = {
-    '2012-12-25' => 'Christmas Day',
-    '2012-12-26' => 'Boxing Day',
-};
-
-my $template = Template->new({ INCLUDE_PATH => 'templates', POST_CHOMP=> 1,});
-# process fullpage template, with events
-$template->process('calendar.tt', {cal => $cal, events => $events, holidays => $holidays })
+# process sidebar template
+my $template->process('sidebar_calendar.tt', {cal => $cal, events => $events })
 || die $template->error();
